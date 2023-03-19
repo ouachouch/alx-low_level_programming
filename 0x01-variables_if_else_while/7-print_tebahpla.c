e20d90e1a254e128250dde7f00bd1fa96215773f
ouachouch <ouachouchobaid@gmail.com>
2023-03-19 23:32:10 +0000

    message

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lettre;
+
+	for (lettre = 'z'; lettre = 'a'; lettre--)
+		putchar(lettre);
+
+	putchar('\n');
+
+	return (0);
+}
