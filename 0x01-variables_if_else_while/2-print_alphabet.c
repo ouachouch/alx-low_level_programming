6c4bd026fd3b6b793aeed7077aac469f11fa3e88
ouachouch <ouachouchobaid@gmail.com>
2023-03-19 22:54:04 +0000

    fixxx

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lettre;
+
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+		putchar(lettre);
+
+	putchar("\n");
+
+	return (0);
+}
