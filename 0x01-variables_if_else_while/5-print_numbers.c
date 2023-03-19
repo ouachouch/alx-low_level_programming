6d0a51744f46162dd23aeb0228444fe4cb00b2cf
ouachouch <ouachouchobaid@gmail.com>
2023-03-19 23:19:17 +0000

    message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 10; i++)
+		printf("%d", n);
+
+	printf("\n");
+
+	return (0);
+}
