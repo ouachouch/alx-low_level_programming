b8ed660be4bab1843a71f5433f1baac590086c1e
ouachouch <ouachouchobaid@gmail.com>
2023-03-19 23:02:51 +0000

    message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	char lettre;
 
-        for (lettre = 'a'; lettre <= 'z'; lettre++)
-                putchar(lettre);
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+		putchar(lettre);
 
 	for (lettre = 'A'; lettre <= 'Z'; lettre++)
 		putchar(lettre);
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
